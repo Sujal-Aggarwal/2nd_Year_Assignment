bee4b8b25fd0f430501e2797d24766c7e0aab6e2
SujalAggarwal <97459859+Sujal-Aggarwal@users.noreply.github.com>
2022-09-24 23:29:36 +0530

    Update Family Linked List -21105036(output).cpp

--- Family Linked List-21105036/Family Linked List -21105036(output).cpp
+++ Family Linked List-21105036/Family Linked List -21105036(output).cpp
@@ -5,15 +5,15 @@ Install the latest PowerShell for new features and improvements! https://aka.ms/
 
 PS D:\Practice> cd "d:\Practice\" ; if ($?) { g++ ClassPractice.cpp -o ClassPractice } ; if ($?) { .\ClassPractice }
 Enter Number Of Family Members:4
-Enter Family Member 1 Name:Bagender Aggarwal  
+Enter Family Member 1 Name:Father  
 Enter Family Member 1 Age:49
-Enter Family Member 2 Name:Kamlesh Aggarwal
+Enter Family Member 2 Name:Mother
 Enter Family Member 2 Age:47
-Enter Family Member 3 Name:Rishita Aggarwwal
+Enter Family Member 3 Name:Sister
 Enter Family Member 3 Age:21
-Enter Family Member 4 Name:Sujal Aggarwal
+Enter Family Member 4 Name:Brother
 Enter Family Member 4 Age:18
 
 Doubly Linked list is shown below:
 
-[Name:Bagender Aggarwal  Age:49]<=>[Name:Kamlesh Aggarwal Age:47]<=>[Name:Rishita Aggarwwal Age:21]<=>[Name:Sujal Aggarwal Age:18]
+[Name:Father  Age:49]<=>[Name:Mother Age:47]<=>[Name:Sister Age:21]<=>[Name:Brother Age:18]
