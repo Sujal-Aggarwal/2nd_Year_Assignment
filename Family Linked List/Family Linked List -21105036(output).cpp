bb8a2aaa2463f732d6696c4111208821dc4e6a07
SujalAggarwal <97459859+Sujal-Aggarwal@users.noreply.github.com>
2022-09-24 20:53:20 +0530

    Create Family Linked List -21105036(output).cpp

---
+++ Family Linked List/Family Linked List -21105036(output).cpp
@@ -0,0 +1,19 @@
+Windows PowerShell
+Copyright (C) Microsoft Corporation. All rights reserved.
+
+Install the latest PowerShell for new features and improvements! https://aka.ms/PSWindows
+
+PS D:\Practice> cd "d:\Practice\" ; if ($?) { g++ ClassPractice.cpp -o ClassPractice } ; if ($?) { .\ClassPractice }
+Enter Number Of Family Members:4
+Enter Family Member 1 Name:Bagender Aggarwal  
+Enter Family Member 1 Age:49
+Enter Family Member 2 Name:Kamlesh Aggarwal
+Enter Family Member 2 Age:47
+Enter Family Member 3 Name:Rishita Aggarwwal
+Enter Family Member 3 Age:21
+Enter Family Member 4 Name:Sujal Aggarwal
+Enter Family Member 4 Age:18
+
+Doubly Linked list is shown below:
+
+[Name:Bagender Aggarwal  Age:49]<=>[Name:Kamlesh Aggarwal Age:47]<=>[Name:Rishita Aggarwwal Age:21]<=>[Name:Sujal Aggarwal Age:18]
