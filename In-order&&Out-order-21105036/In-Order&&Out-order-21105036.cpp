35b02865ac82575ad3c1c49fcf71a9265b7e6456
SujalAggarwal-21105036 <97459859+sujalaggarwal21105036@users.noreply.github.com>
2022-10-20 22:45:10 +0530

    Create In-Order&&Out-order-21105036.cpp

---
+++ In-order&&Out-order-21105036/In-Order&&Out-order-21105036.cpp
@@ -0,0 +1,15 @@
+Windows PowerShell
+Copyright (C) Microsoft Corporation. All rights reserved.
+
+Install the latest PowerShell for new features and improvements! https://aka.ms/PSWindows
+
+PS D:\C++Programming\A2OJ_Ladder\Rating_less_than_1300> cd "c:\Users\sujal\Desktop\code\" ; if ($?) { g++ 1.cpp -o 1 } ; if ($?) { .\1 }
+Enter the size of the array
+6
+Kindly enter the values of the array
+7 8 9 3 6 1
+Kindly decide the algorithm using which you want to sort your array :
+Enter 0 for in-place algorithm and 1 for out-place algorithm :
+0
+For in-place algorithm your sorted array is :
+1 3 6 7 8 9
